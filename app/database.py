433401e6de6b0a2ad51857ecf88a4f74b0907124
Microsoft Learn Student <learn@contoso.com>
2022-08-31 10:27:01 -0700

    fixed env variables

--- app/database.py
+++ app/database.py
@@ -3,9 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres@localhost:5432/todo-app"
 # AZURE_DATABASE_URL = f"postgresql://{os.getenv('DATABASE_USER')}:{os.getenv('DATABASE_PASSWORD')}@{os.getenv('DATABASE_HOST')}/{os.getenv('DATABASE_NAME')}?sslmode=require"
-EB_DATABASE_URI = os.getenv('DATABASE_URI')
+EB_DATABASE_URI = os.getenv('DATABASE_URI', "postgresql://postgres@localhost:5432/todo-app")
 
 # connect_args={"check_same_thread": False} [for Sqlite only]
 engine = create_engine(EB_DATABASE_URI)
